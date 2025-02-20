3ad1a045db83ef4d60626d3a7911c1baec3c6a43
stannyweb <stannyweb@gmail.com>
2025-02-20 18:48:39 +0200

    Add test using Explicit Wait
    - Added a new test to demonstrate the use of Explicit Wait.
    - Waited for a specific condition before proceeding with the test.
    - Included assertions to verify the expected behavior after the wait.

--- src/main/java/pages/DynamicLoadingPage.java
+++ src/main/java/pages/DynamicLoadingPage.java
@@ -5,14 +5,20 @@ import org.openqa.selenium.WebDriver;
 
 public class DynamicLoadingPage {
     private WebDriver driver;
-    private By dynamicLink = By.linkText("Example 1: Element on page that is hidden");
+    private By example1 = By.linkText("Example 1: Element on page that is hidden");
+    private By example2 = By.linkText("Example 2: Element rendered after the fact");
 
     public DynamicLoadingPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public DynamicLoadingExample1 dynamicExample1() {
-        driver.findElement(dynamicLink).click();
+        driver.findElement(example1).click();
         return new DynamicLoadingExample1(driver);
+    }
+
+    public DynamicLoadingExample2 dynamicExample2() {
+        driver.findElement(example2).click();
+        return new DynamicLoadingExample2(driver);
     }
 }
--- src/test/java/wait/WaitTests.java
+++ src/test/java/wait/WaitTests.java
@@ -4,6 +4,7 @@ import base.BaseTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.DynamicLoadingExample1;
+import pages.DynamicLoadingExample2;
 import pages.DynamicLoadingPage;
 
 public class WaitTests extends BaseTests {
@@ -14,5 +15,13 @@ public class WaitTests extends BaseTests {
         dynamicLoading.clickStart();
 
         Assert.assertEquals(dynamicLoading.getLoadedText(), "Hello World!", "Incorrect dynamic loading text");
+    }
+
+    @Test
+    public void waitUntilTextIsVisible() {
+        DynamicLoadingExample2 dynamicLoading = homePage.clickDynamicLoading().dynamicExample2();
+        dynamicLoading.clickStart();
+
+        Assert.assertEquals(dynamicLoading.getLoadedText(), "Hello World!", "Incorrect loaded text");
     }
 }
